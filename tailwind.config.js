adab16e057a95028ac1cae72cbb22a2f86529a2d
Vidhi Gupta <gupta_vidhi@outlook.com>
2025-09-14 23:16:34 +0530

    basic good looking webcv

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // <-- add this line
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
